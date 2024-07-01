3a9cb6396f8dd208ef814021f36357db20817206
Kang Liao <88041650+KangLiao929@users.noreply.github.com>
2024-07-01 13:51:06 +0800

    Update test_portrait.py

--- test_portrait.py
+++ test_portrait.py
@@ -6,7 +6,7 @@ import os
 import glob
 import numpy as np
 import cv2
-from Code.MOWA.Github.model.network import MOWA
+from model.network import MOWA
 from collections import OrderedDict
 from utils.utils_op import *
 from tqdm import tqdm
